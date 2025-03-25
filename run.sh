4d9f6a426371e5765910205b5ff2730911c0e67f
Ziyu <ziyuye@uchicago.edu>
2025-03-24 23:19:45 -0500

    fix qwen

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
-export WANDB_PROJECT=qwen_test
+export WANDB_PROJECT=qwen2.5-vl-it-sft
+
+# ACCELERATE_LOG_LEVEL=info \
+#     accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml \
+#     scripts/run_sft.py \
+#     recipes/zephyr-7b-gemma/sft/config_full.yaml \
+#     --report_to=wandb \
 
 ACCELERATE_LOG_LEVEL=info \
     accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml \
     scripts/run_sft.py \
-    recipes/zephyr-7b-gemma/sft/config_full.yaml \
+    recipes/qwen/sft/config_full.yaml \
     --report_to=wandb \
