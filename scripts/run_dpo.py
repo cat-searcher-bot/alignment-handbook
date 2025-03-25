033146594262c530b45f1f18a8ab9ea155882a9d
Ziyu <ziyuye@uchicago.edu>
2025-03-24 23:23:33 -0500

    fix: model

--- scripts/run_dpo.py
+++ scripts/run_dpo.py
@@ -229,9 +229,8 @@ def main():
 
     # Save everything else on main process
     kwargs = {
-        "finetuned_from": model_args.model_name_or_path,
-        "dataset": list(data_args.dataset_mixer.keys()),
-        "dataset_tags": list(data_args.dataset_mixer.keys()),
+        "model_name": model_args.model_name_or_path,
+        "dataset_name": list(data_args.dataset_mixer.keys()),
         "tags": ["alignment-handbook"],
     }
     if trainer.accelerator.is_main_process:
